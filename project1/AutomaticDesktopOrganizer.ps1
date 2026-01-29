3966b1c2135b98a58d006d8fe8cc68343671d9b8
kingyost <31832631+kingyost@users.noreply.github.com>
2026-01-28 19:07:47 -0500

    jan 28, more planning/learning

---
+++ project1/AutomaticDesktopOrganizer.ps1
@@ -0,0 +1,20 @@
+# set the target folder to sort
+$targetPath = "c:\User\KingYost\Documents\Github\Project1\ToSortFolder"
+
+# get a list of items and their file type from the ToSortFolder
+
+
+<#
+For loop, sorting all of the files into their respective subfolders
+jpeg/png -> images, txt -> text, anything else -> other
+This is a multi line comment
+#>
+
+ # determine file type for sorting
+
+ # check that the nessessary folder exits
+
+ # move file
+
+
+# Declare to user that the task is complete
