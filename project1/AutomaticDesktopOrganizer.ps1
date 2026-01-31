54ef9d2cc6717aa20db8f4d818f9282855b4ad6e
kingyost <31832631+kingyost@users.noreply.github.com>
2026-01-31 08:55:42 -0500

    - get list of files - write exit statement - add if-else for for-loop

--- project1/AutomaticDesktopOrganizer.ps1
+++ project1/AutomaticDesktopOrganizer.ps1
@@ -2,19 +2,35 @@
 $targetPath = "c:\User\KingYost\Documents\Github\Project1\ToSortFolder"
 
 # get a list of items and their file type from the ToSortFolder
-
+$fileList = @(Get-childitem | select-object name, extension)
 
 <#
 For loop, sorting all of the files into their respective subfolders
 jpeg/png -> images, txt -> text, anything else -> other
 This is a multi line comment
 #>
+if ($fileList.length -gt 0){
+# determine file type
+  for (){
 
- # determine file type for sorting
+  }
+# if-else statement to select folder
 
- # check that the nessessary folder exits
+# check that the nessessary folder exits
 
- # move file
+# move file
+
+}
+else{
+  write-host "There are no files in $targetPath" -ForegroundColor Green
+}
 
 
 # Declare to user that the task is complete
+# use multiline string from var
+$exitStatement = @"
+All files sorted!
+There were $fileList.length in $targetPath
+"
+
+write-host "$exitStatement" -ForegroundColor Green
