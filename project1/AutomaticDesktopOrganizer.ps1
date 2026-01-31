d83f867ee363c9f3400ec7bbb933af0153217c4d
kingyost <31832631+kingyost@users.noreply.github.com>
2026-01-31 16:50:24 -0500

    fixed issues with moving files

--- project1/AutomaticDesktopOrganizer.ps1
+++ project1/AutomaticDesktopOrganizer.ps1
@@ -1,24 +1,49 @@
 # set the target folder to sort
-$targetPath = "c:\User\KingYost\Documents\Github\Project1\ToSortFolder"
+$targetPath = ".\ToSortFolder"
 
 # get a list of items and their file type from the ToSortFolder
-$fileList = @(Get-childitem | select-object name, extension)
+$fileList = @(Get-childitem -File -Path $targetPath)
+# $fileList = @(Get-childitem -File -Path ".\ToSortFolder")
+
+#debug
+# write-host $fileList.count
+
+#get count to report later
+#not sure what the issue is but can't get it to report at the end
+#using $fileList.count with (), [], or {}. moving on
+$count = $fileList.count
 
 <#
 For loop, sorting all of the files into their respective subfolders
 jpeg/png -> images, txt -> text, anything else -> other
 This is a multi line comment
 #>
-if ($fileList.length -gt 0){
-# determine file type
-  for (){
 
+if ($fileList.count -gt 0){
+# loop
+  foreach ($file in $fileList){
+
+    # determine file type
+    $fileType = $file.extension
+
+    # if-else statement to select folder and move item
+    if( $fileType -match ".txt"){
+      move-item $file.FullName -destination .\ToSortFolder\TxtFiles\
+    }
+    elseif ($fileType -match ".jpeg" -or $fileType -match ".jpg" -or $fileType -match ".png"){
+      move-item $file.FullName -destination .\ToSortFolder\Images\
+    }
+    else{
+      move-item $file.FullName -destination .\ToSortFolder\Other\
+    }
   }
-# if-else statement to select folder
 
-# check that the nessessary folder exits
-
-# move file
+  # $exitStatement = @"
+  # All files sorted!
+  # There were $fileList.count in $targetPath
+  # "@
+  #
+  # write-host "$exitStatement" -ForegroundColor Green
 
 }
 else{
@@ -27,10 +52,10 @@ else{
 
 
 # Declare to user that the task is complete
-# use multiline string from var
+
 $exitStatement = @"
 All files sorted!
-There were $fileList.length in $targetPath
-"
+There were $count in $targetPath
+"@
 
 write-host "$exitStatement" -ForegroundColor Green
