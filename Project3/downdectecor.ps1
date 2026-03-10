e5b61add10bf87269fdff949e5055f0a9eeca7bc
kingyost <31832631+kingyost@users.noreply.github.com>
2026-03-10 19:35:34 -0400

    made menu

---
+++ Project3/downdectecor.ps1
@@ -0,0 +1,18 @@
+function show-menu {
+  Clear-Host
+  Write-Host "===========================" -ForegroundColor Cyan`
+  Write-Host "   Website Downdetector    " -ForegroundColor White
+  Write-Host "===========================" -ForegroundColor Cyan
+  Write-Host "1) Google"
+  Write-Host "2) Cruncyroll"
+  Write-Host "3) Steam"
+  Write-Host "4) Other"
+  Write-Host "Q) Quit"
+  Write-Host "===========================" -ForegroundColor Cyan`
+}
+
+$menu = ""
+
+while ($menu -ne "Q"){
+
+}
