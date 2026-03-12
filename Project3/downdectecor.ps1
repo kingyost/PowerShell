c437443cb92139bc59f391a61694ee59b5dd2450
kingyost <31832631+kingyost@users.noreply.github.com>
2026-03-11 20:08:15 -0400

    another menu and a switch

--- Project3/downdectecor.ps1
+++ Project3/downdectecor.ps1
@@ -1,18 +1,78 @@
+#variables
+$menu = ""
+$other = ""
+$email = false
+
+#Functions
 function show-menu {
-  Clear-Host
-  Write-Host "===========================" -ForegroundColor Cyan`
-  Write-Host "   Website Downdetector    " -ForegroundColor White
-  Write-Host "===========================" -ForegroundColor Cyan
-  Write-Host "1) Google"
-  Write-Host "2) Cruncyroll"
-  Write-Host "3) Steam"
-  Write-Host "4) Other"
-  Write-Host "Q) Quit"
-  Write-Host "===========================" -ForegroundColor Cyan`
+  clear-host
+  write-host "===========================" -ForegroundColor Cyan`
+  write-host "   Website Downdetector    " -ForegroundColor White
+  write-host "===========================" -ForegroundColor Cyan
+  write-host "1) Google"
+  write-host "2) Cruncyroll"
+  write-host "3) Steam"
+  write-host "4) Facebook"
+  write-host "5) Other"
+  write-host "S) Settings"
+  write-host "Q) Quit"
+  write-host "===========================" -ForegroundColor Cyan`
 }
 
-$menu = ""
+function setting-menu{
+  clear-host
+  write-host "===========================" -ForegroundColor Cyan`
+  write-host "         Settings          " -ForegroundColor White
+  write-host "===========================" -ForegroundColor Cyan
+  write-host "Would you like the results emailed to you?"
+  write-host "1) Yes"
+  write-host "2) No"
+}
 
+function ping-website{
+  param (
+    [string]$website
+  )
+  write-host "Checking on $website"
+  ping
+}
+
+function email-results{
+
+}
+
+#The Juice
 while ($menu -ne "Q"){
+  show-menu
+  $menu = read-host "Please make a Selection"
+
+  switch ($menu){
+    "1"{
+
+    }
+    "2"{
+
+    }
+    "3"{
+
+    }
+    "4"{
+
+    }
+    "5"{
+
+    }
+    "S"{
+
+    }
+    Default {
+      write-host""
+    }
+  }
+
+  if($menu -ne "Q"){
+    read-host "`nPress Enter to return to menu"
+  }
+
 
 }
